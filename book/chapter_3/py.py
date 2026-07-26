5758ca44006612800cefb1ce379eedcd448988f7
KonuhovAND <andreykonuhov8@gmail.com>
2026-07-26 18:54:01 +0300

    +1 hour.tomorrow its time to practice

--- book/chapter_3/py.py
+++ book/chapter_3/py.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import sklearn.model_selection
 from sklearn.datasets import load_digits
 from sklearn.linear_model import SGDClassifier
@@ -62,3 +63,21 @@ for train_index,test_index in skfolds.split(X_train,y_train_7):
 # >>>0.9937369519832986
 # >>>0.9853862212943633
 # >>>0.9916492693110647
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_predict
+
+y_prediction = cross_val_predict(sdg_clf,X_train,y_train_7,cv=3)
+print(f"Confusion matrix of prediction is:\n {confusion_matrix(y_train_7,y_prediction)}")
+#>>> [[1284    8]
+#>>> [   6  139]]
+
+from sklearn.metrics import roc_curve
+
+y_prediction = cross_val_predict(sdg_clf,X_train,y_train_7,cv=3,method='decision_function')
+fpr,tpr,thresholds = roc_curve(y_train_7,y_prediction)
+plt.plot(fpr,tpr,linewidth =2,label="ROC curve")
+plt.plot([0,1],[0,1],'k:',label="Random clasifier\' Roc curve")
+
+plt.savefig('book/chapter_3/ROC.png')
+
