ebb8c4926b98a595af018d7aa6dbe56bb52bac9b
KonuhovAND <andreykonuhov8@gmail.com>
2026-08-01 20:01:29 +0300

    done 3rd task

--- book/chapter_3/homework.py
+++ book/chapter_3/homework.py
@@ -1,3 +1,9 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+
+
 def exercise_1():
     from sklearn.datasets import load_digits
     from sklearn.metrics import accuracy_score
@@ -28,7 +34,7 @@ def exercise_1():
     preds = searcher.predict(x_test)
     print(accuracy_score(y_test, preds))
     print(searcher.best_params_, searcher.best_score_)
-    print("=" * 30)
+    print("=*" * 40)
     print("task 2")
 
     x_train_huge, x_test_huge, y_train_huge, y_test_huge = exercise_2()
@@ -36,15 +42,20 @@ def exercise_1():
     preds = searcher.predict(x_test_huge)
     print(accuracy_score(y_test_huge, preds))
 
+    print("=*" * 40)
+    print("task 3")
+    pred = exercise_3()
+    print(pred[:100])
+
 
 def exercise_2():
     # TODO: Write a function that can shift an MNIST image in any direction (left, right, up, or down) by one pixel.
     # TODO: For each image in the training set, create four shifted copies (one per direction) and add them to the training set.
     # TODO: Train your best model on this expanded training set and measure its accuracy on the test set.
-    from sklearn.datasets import load_digits
-    from scipy.ndimage import shift
-    from sklearn.model_selection import train_test_split
     import numpy as np
+    from scipy.ndimage import shift
+    from sklearn.datasets import load_digits
+    from sklearn.model_selection import train_test_split
 
     Data = load_digits(as_frame=False)
     x, y = Data.images, Data.target
@@ -73,7 +84,45 @@ def exercise_3():
     # TODO: Download the data from https://homl.info/titanic.tgz and unzip it.
     # TODO: Load train.csv and test.csv using pandas.read_csv().
     # TODO: Train a classifier that predicts the Survived column based on other columns.
-    pass
+    from pandas import read_csv
+    from sklearn.compose import ColumnTransformer
+    from sklearn.preprocessing import OneHotEncoder
+
+    train, test = (
+        read_csv("/home/andreyk/PythonML/book/chapter_3/train.csv"),
+        read_csv("~/PythonML/book/chapter_3/test.csv"),
+    )
+    y_train, x_train = train["Survived"], train.drop(["Survived"], axis=1)
+    x_train = x_train.drop(["Name", "Ticket", "Cabin", "PassengerId"], axis=1)
+
+    x_test = test.drop(["Name", "Ticket", "Cabin", "PassengerId"], axis=1)
+
+    num_cols = ["Age", "Fare", "SibSp", "Parch"]
+    cat_cols = ["Sex", "Embarked", "Pclass"]
+
+    # Preprocessing
+    num_pipe = Pipeline(
+        [("imputer", SimpleImputer(strategy="median")), ("scaler", StandardScaler())]
+    )
+
+    cat_pipe = Pipeline(
+        [
+            ("imputer", SimpleImputer(strategy="most_frequent")),
+            ("encoder", OneHotEncoder(handle_unknown="ignore")),
+        ]
+    )
+
+    preprocessor = ColumnTransformer(
+        [("num", num_pipe, num_cols), ("cat", cat_pipe, cat_cols)]
+    )
+
+    full_pipeline = Pipeline(
+        [("preprocess", preprocessor), ("knn", KNeighborsClassifier(n_neighbors=5))]
+    )
+
+    full_pipeline.fit(x_train, y_train)
+    pred = full_pipeline.predict(x_test)
+    return pred
 
 
 def exercise_4():
