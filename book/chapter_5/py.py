7ee6690f15009a580aedcfd023210965924cb8b8
KonuhovAND <andreykonuhov8@gmail.com>
2026-08-16 19:34:53 +0300

    read couple pages of chapter 5

---
+++ book/chapter_5/py.py
@@ -0,0 +1,23 @@
+from graphviz import Source
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+iris = load_iris(as_frame=True)
+x, y= iris.data[['petal length (cm)', 'petal width (cm)']].values,iris.target
+
+
+dtr_clf = DecisionTreeClassifier(random_state=42,max_depth=2)
+dtr_clf.fit(x,y)
+
+# export_graphviz(
+#     dtr_clf,
+#     out_file='my.dot',
+#     feature_names=['petal length (cm)','petal width (cm)'],
+#     class_names = iris.target_names,
+#     rounded=True,
+#     filled=True
+# )
+
+# Source.from_file('my.dot').render('my.dot', view=True)
+
+print(dtr_clf.predict_proba([[5, 1.5]]).round(3))
