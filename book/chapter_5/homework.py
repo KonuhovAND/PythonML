6e1fce11e1a234930bd952f2025960b1aa4bd971
KonuhovAND <andreykonuhov8@gmail.com>
2026-08-25 14:57:16 +0300

    1/2 tasks for handson ml chapter 5 disicion tree

---
+++ book/chapter_5/homework.py
@@ -0,0 +1,46 @@
+# 7. Train and fine-tune a decision tree for the moons dataset by following these
+# steps:
+# a. Use make_moons(n_samples=10000, noise=0.4) to generate a moons dataset.
+# b. Use train_test_split() to split the dataset into a training set and a test set.
+# c. Use grid search with cross-validation (with the help of the GridSearchCV
+# class) to find good hyperparameter values for a DecisionTreeClassifier.
+# Hint: try various values for max_leaf_nodes.
+# d. Train it on the full training set using these hyperparameters, and measure
+# your model’s performance on the test set. You should get roughly 85% to 87%
+# accuracy.
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+X, y = make_moons(n_samples=1000, noise=0.4, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, random_state=42, test_size=0.2
+)
+
+param_grid = {
+    "max_depth": [3, 5, 7, 10],
+    "max_leaf_nodes": [5, 10, 12, 15, 20, 30, None],
+    "min_samples_split": [2, 3, 4, 6],
+    "max_features": [1, 2, 4, 6, 8],
+}
+params_dicision= {'max_depth': 3, 'max_features': 1, 'max_leaf_nodes': None, 'min_samples_split': 2}
+grid = GridSearchCV(DecisionTreeClassifier(random_state=42), param_grid)
+grid.fit(X_train, y_train)
+print(grid.best_score_, grid.best_params_)
+
+
+# 8. Grow a forest by following these steps:
+# a. Continuing the previous exercise, generate 1,000 subsets of the training set,
+# each containing 100 instances selected randomly. Hint: you can use ScikitLearn’s ShuffleSplit class for this.
+# b. Train one decision tree on each subset, using the best hyperparameter values
+# found in the previous exercise. Evaluate these 1,000 decision trees on the test
+# set. Since they were trained on smaller sets, these decision trees will likely
+# perform worse than the first decision tree, achieving only about 80% accuracy.
+# Exercises | 193
+# c. Now comes the magic. For each test set instance, generate the predictions of
+# the 1,000 decision trees, and keep only the most frequent prediction (you can
+# use SciPy’s mode() function for this). This approach gives you majority-vote
+# predictions over the test set.
+# d. Evaluate these predictions on the test set: you should obtain a slightly higher
+# accuracy than your first model (about 0.5 to 1.5% higher). Congratulations,
+# you have trained a random forest classifier!
