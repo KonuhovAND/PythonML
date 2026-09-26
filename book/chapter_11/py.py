6f30bc05082fd9c0db2672ad8e4c948c661a2c73
KonuhovAND <andreykonuhov8@gmail.com>
2026-09-26 22:04:32 +0300

    new new

--- book/chapter_11/py.py
+++ book/chapter_11/py.py
@@ -2,5 +2,6 @@ import torch
 import torch.nn as nn
 
 layer = nn.Linear(40, 10)
-layer.weight.data *= 6**0.5
-torch.zero_(layer.bias.data)
+nn.init.kaiming_uniform_(layer.weight)
+nn.init.zeros_(layer.bias)
+print(layer.bias.data)
